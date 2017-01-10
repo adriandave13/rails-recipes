467646e2f70e8270e7223b2705e1c686aa5c6e57
adriandave13 <adrian@paradoxlogic.co>
2017-01-10 00:23:51 -0500

    Updated Recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,54 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def edit
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      flash[:notice] = "Recipe was successfully created."
+      redirect_to @recipe
+    else
+      render :action => 'new'
+    end
+  end
+
+  def update
+    @recipe = Recipe.update(recipe_params)
+    if @recipe.save
+      flash[:notice] = "Recipe was successfully updated."
+      redirect_to @recipe
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    flash[:notice] = "Recipe was successfully destroyed."
+    redirect_to recipes_url
+  end
+
+  private
+  
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+    def recipe_params
+      params.require(:recipe).permit(:name, :instructions)
+    end
+
 end
