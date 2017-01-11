bdf6f76dd5bca1b4a340e82dacd9c601ef167955
adriandave13 <adrian@paradoxlogic.co>
2017-01-11 17:50:29 -0500

    Updated Devise configuration and navigation bar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111221122) do
+ActiveRecord::Schema.define(version: 20170111224457) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20170111221122) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
